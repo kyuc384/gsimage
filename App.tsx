c71b16626b056f8172a72c6fd1d629ec4e2e4397
kyuc384 <113677956+kyuc384@users.noreply.github.com>
2026-03-13 14:26:12 +0700

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -187,7 +187,7 @@ const App: React.FC = () => {
       imageZoom: 1,
       extractedPrompt: '',
       manualPrompt: '',
-      selectedStyleId: 'cinematic',
+      selectedStyleId: 'photorealistic',
       variants: [],
       status: AppStatus.IDLE,
       error: null,
